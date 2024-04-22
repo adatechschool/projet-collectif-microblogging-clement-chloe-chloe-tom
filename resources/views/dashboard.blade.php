becc0052455d0304d9009a919732c43bebee24b3
Clément Coadou <114520203+Kushumai@users.noreply.github.com>
2024-04-22 15:02:38 +0000

    add post into dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,14 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
+
+                @foreach ($posts as $post)
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h2>{{ $post->title }}</h2>
+                    <p>{{ $post->content }}</p>
+                </div>
+            @endforeach
+
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
+namespace App\Http\Controllers;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 
@@ -20,17 +21,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/posts', [PostController::class,'index'])->name('posts');
-Route::get('/posts/{post}', [PostController::class,'show'])->name('posts.show');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/posts', [PostController::class,'index'])->name('posts');
+    Route::get('/posts/{post}', [PostController::class,'show'])->name('posts.show');
 });
 
 require __DIR__.'/auth.php';
