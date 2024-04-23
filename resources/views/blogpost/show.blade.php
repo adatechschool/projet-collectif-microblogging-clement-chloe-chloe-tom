d157fba233b78ec569da8e6d97f61f55e3c18486
Stoyots <tom.benchetrit@gmail.com>
2024-04-23 17:02:15 +0200

    modifications viseuelles

--- resources/views/blogpost/show.blade.php
+++ resources/views/blogpost/show.blade.php
@@ -1,9 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-red leading-tight">
+            {{ __('Post') }}
+        </h2>
+    </x-slot>
+
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 <body class="bg-gray-200">
 
-<div class="mt-40 max-w-md mx-auto bg-white border border-gray-200 rounded-lg shadow-lg p-6 text-center transition-transform transform hover:scale-110 hover:shadow-xl">
-    <h3 class="text-2xl font-bold text-gray-800 mb-4">{{$post->title}}</h3>
+<div class="m-10 mb-10 mr-10 ml-10 p-6 text-black border border-teal-600 rounded-lg bg-white transform hover:scale-105 hover:bg-amber-50 hover:border-4 hover:border-blue-500 transition-all duration-200">
+    <p class="text-2xl font-bold text-gray-800 mb-4">{{$post->title}}</p>
     <p class="text-gray-600 mb-6">{{$post->content}}</p>
     <p class="text-gray-600 mb-6">Posté par: <a href="{{ route('user.show', ['user_id' => $post->user->id]) }}" class="text-blue-500 hover:text-blue-700">{{$post->user->name}}</a></p>
-    <a href="/posts" class="text-blue-500 hover:text-blue-700">Retourner aux articles</a>
+    <a href="/posts" class="text-blue-500 hover:text-blue-700">Retourner au Dashboard</a>
 </div>
+</x-app-layout>
--- resources/views/user_posts.blade.php
+++ resources/views/user_posts.blade.php
@@ -1,3 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Mur') }}
+        </h2>
+    </x-slot>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -14,7 +21,7 @@
         <div class="space-y-6">
             @forelse ($posts as $post)
                 <!-- Chaque section de post a un effet d'agrandissement au hover -->
-                <a href="/posts/{{$post->id}}" class="block p-6 bg-white border border-gray-200 rounded-lg shadow transition-transform transform hover:scale-110 hover:shadow-lg hover:bg-gray">
+                <a href="/posts/{{$post->id}}" class="block p-6 bg-white border border-blue-200 rounded-lg shadow transition-transform transform hover:scale-105 hover:bg-amber-50 hover:border-4 hover:border-blue-500 transition-all duration-200">
                     <h3 class="text-xl font-semibold text-blue-600 hover:text-blue-800">{{$post->title}}</h3>
                     <p class="text-gray-600 mt-4">{{$post->content}}</p>
                     <p class="text-gray-400 mb-6 text-xs italic ">Posté par: {{$post->user->name}}</p> <!-- Ajout du nom de l'utilisateur -->
@@ -26,3 +33,4 @@
     </div>
 </body>
 </html>
+</x-app-layout>
